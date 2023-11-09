8b4779e26fa23c056a4cf1035b5c36bb915ad517
robinreyescodes <robinreyes.code@gmail.com>
2023-11-09 11:52:01 -0800

    #4 exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...d) {
+    //use the spread parameter to get all of the parameters if multiple
+    for (const num of d) {
+    if (!arr.includes(num)) continue;
+    //find the index of our items from d, use that to find the index
+    //we need to delete from array
+    let itemIndex = arr.indexOf(num);
+    //use slice to get rid of the elements from d
+    arr.splice(itemIndex, 1);
+    }
 
+
+    return arr;
 };
 
 // Do not edit below this line
