6889b4d98b06e150f532439a432e46b009014b76
robinreyescodes <robinreyes.code@gmail.com>
2023-11-09 12:07:34 -0800

    sum all exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //our edge cases, return ERRORS for non negatives
+    // and non number types
+    if (a < 0 || b < 0) return "ERROR";
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR";
 
+    let sum = a;
+    if (a < b) {
+        for (let i = a+1; i <= b; i++) {
+            sum+= i;
+        }
+    }
+    if (b < a) {
+        for (let i = a - 1; i >=b; i--) {
+            sum+= i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
