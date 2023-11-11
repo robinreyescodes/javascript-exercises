d1082ba7b9062a4952127fc7674fdfde6b97623d
robinreyescodes <robinreyes.code@gmail.com>
2023-11-10 18:26:39 -0800

    tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (temp - 32) * (5/9);
+
+  return Number(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * 9/5) + 32;
+
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
