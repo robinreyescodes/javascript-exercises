84a04ce76f5688e950b8ecddf9ec3ae127c38e0b
robinreyescodes <robinreyes.code@gmail.com>
2023-11-10 18:09:50 -0800

    finished leapyears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //years divisible by 4 are leap
+    // but CANNOT be divisible by 100
+    // they CAN be divisible by 400 tho...
+    let answer = false;
+    if (year % 4 === 0 && !(year % 100 === 0)) answer = true;
+    if (year % 400 === 0) answer = true;
 
+    return answer;
 };
 
 // Do not edit below this line
