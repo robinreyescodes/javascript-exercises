023623042b3ae06a0a3de784a280cef1c6f5a21a
robinreyescodes <robinreyes.code@gmail.com>
2023-11-18 09:56:01 -0800

    calculator exercise complete:

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 
 };
 
-const power = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const factorial = function() {
-	
+const sum = function(arr) {
+    let current = 0;
+    let sum = arr.reduce((acc, cur) => acc + cur, current);
+    return sum;
+};
+
+const multiply = function(arr) {
+    let sum = 1;
+    let multipliedNums = arr.reduce((acc, cur) => acc * cur, sum);
+    return multipliedNums;
+
+};
+
+const power = function(num, toRaise) {
+    let sum = 1;
+	for (let i = 0; i < toRaise; i++) {
+        sum*= num;
+    }
+    return sum;
+};
+
+const factorial = function(number) {
+    let answer = 1;
+    while(number > 0) {
+        answer *= number;
+        number--;
+    }
+    return answer;
 };
 
 // Do not edit below this line
