d3288e17dad153c8e0f7a21d375c6e3347a67cdd
robinreyescodes <robinreyes.code@gmail.com>
2023-12-04 12:36:00 -0800

    morning warm up

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //pointers
+    let i = 0;
+    let j = word.length-1;
 
+
+    while (i !== j) {
+        if (word[i] !== word[j]) return false;
+        console.log(word[i], word[j]);
+        i++;
+        j--;
+    }
+    return i === j;
 };
 
 // Do not edit below this line
--- DOM/prac.js
+++ DOM/prac.js
@@ -19,13 +19,14 @@ for (const item of itemsArr) {
  use a doc frag to better performance!
  */
 
+/*
 const frag = new DocumentFragment();
 const itemList = document.querySelector("#item-list");
 const languages = ['js', 'python', 'java', 'c++', 'c'];
 
-/*
+/!*
 *
-* */
+* *!/
 
 for (const language of languages) {
     const item = document.createElement("li");
@@ -42,4 +43,40 @@ const items = libs.map((lib) => {
     item.innerText = lib;
     return item;
 });
-console.log(items[0]);
+console.log(items[0]);*/
+
+const username = document.querySelector("#username");
+console.log(username.id);
+console.log(username.type);
+
+/*
+*     element.getAttribute(name) – get the attribute value
+    element.setAttribute(name, value) – set the value for the attribute
+    element.hasAttribute(name) – check for the existence of an attribute
+    element.removeAttribute(name) – remove the attribute
+*
+* */
+
+console.log(username.getAttribute("name"));
+username.setAttribute("value", "hihijhi");
+console.log(username.value);
+
+for (const attribute of username.attributes) {
+    console.log(`${attribute.name}: ${attribute.value}`)
+}
+
+console.log(username.dataset);
+
+console.log('--------------')
+const myButton = document.querySelector("#my-button");
+myButton.setAttribute("name", "send");
+myButton.setAttribute("value", "penis");
+
+console.log(myButton.getAttribute("name"));
+console.log(myButton.getAttribute("value"));
+
+myButton.removeAttribute("name");
+console.log(myButton);
+
+if (myButton.hasAttribute(name)) console.log('here');
+else console.log('name is not here yet');
