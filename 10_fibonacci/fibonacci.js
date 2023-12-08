d5f0d582005d9dd4f38b7b5e11640b82de309c09
robinreyescodes <robinreyes.code@gmail.com>
2023-12-08 12:25:11 -0800

    fibo exercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let j = 0;
+    let k = 1;
+    let sum;
+
+    if (num < 0) return "OOPS";
+
+    for (let i = 0; i < num; i++) {
+        sum = j + k;
+        j = k;
+        k = sum;
+    }
+
+
+
+    return j;
 
 };
 
