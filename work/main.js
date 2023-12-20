47f08e9d923941b864e3c49f8c8c40b3e3b951bb
robinreyescodes <robinreyes.code@gmail.com>
2023-12-20 10:24:48 -0800

    making a library app

--- work/main.js
+++ work/main.js
@@ -1,113 +1,69 @@
-//working with objects and OOP
+const myLibrary = [];
 
-//what is a constructor function?
-class Car {
-    set setSpeed(speed) {
-        this.speed = speed;
-    }
+//variable declarations
+const container = document.querySelector(".container");
+const button = document.querySelector("#submit-btn");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
 
-    constructor(name, doors) {
+addBookToLibrary("HP & the Sorceror's Stone", "J.K. Rowling", 201);
+addBookToLibrary("Mr. Mercedes", "Stephen King", 297);
 
-        this.name = name;
-        this.doors = doors;
-    }
 
-    get getSpeed() {
-        return this.speed;
-    }
+button.addEventListener('click', e => {
+    e.preventDefault();
+    //print all of the values to the console
+    console.log(title.value);
+    console.log(author.value);
+    console.log(pages.value);
+    //clear all of the inputs
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    //focus on the title of the page
+    title.focus();
 
-    accelerate(){
-        this.speed += 10;
-    }
+})
+
+
+//loop through the library array and print the books that are inside
+for (const book of myLibrary) {
+    const card = document.createElement("div");
+    const titleCard = document.createElement('p');
+    const authorCard = document.createElement('p');
+    const pagesCard = document.createElement('p');
+
+    card.className = 'card';
+    titleCard.className = 'title';
+    authorCard.className = 'author';
+    pagesCard.className = 'pages';
+
+    titleCard.textContent = "title: " + book.title;
+    authorCard.textContent = "author: " + book.author;
+    pagesCard.textContent = "pages: " + book.pages;
+
+    card.appendChild(titleCard);
+    card.appendChild(authorCard);
+    card.appendChild(pagesCard);
+
+    container.appendChild(card);
 }
 
-class EV extends Car {
-    #charge = 100;
-    //technically do not need this constructor because i'm not adding new things through this
-    constructor(name, doors) {
-        super(name, doors)
-    }
 
-    get getCharge() {
-        return this.#charge;
-    }
-    accelerate() {
-        this.speed += 5;
-        this.#charge -= 5;
-        return this;
-    }
 
-    chargeBattery() {
-        this.#charge += 20;
-        return this;
-    }
 
+
+
+
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
-const m3 = new Car("BMW M3", 4);
-
-const model3 = new EV("tesla model 3", 4);
-
-
-model3.setSpeed = 100;
-
-
-/*
-* Polymorphism and inheritance is in effect here.
-* The parent class 'Car' has setSpeed, getSpeed and accelerate. Since the EV class 'extends'
-* the Car class, EV objects can now use the functions that are already in the Car class.
-* Inheritance! Now, this is where Polymorphism comes in: the EV class can also have the same
-* name functions, but now updated for itself.
-* */
-console.log(model3.getSpeed);
-
-m3.accelerate();
-model3.accelerate();
-
-console.log(`m3 speed: ${m3.getSpeed}`)
-console.log(`model 3 speed: ${model3.getSpeed}, charge: ${model3.getCharge}`)
-
-model3.accelerate().accelerate().chargeBattery();
-console.log(model3.getCharge);
-
-
-
-console.log("");
-// new example with protected properties
-
-/*
-class Account {
-    constructor(name, currency, pin) {
-        this.name = name;
-        this.currency = currency;
-        //protected
-        this._pin = pin;
-        this._movements = [];
-
-
-        console.log(`welcome ${this.name}, thank you for opening an account`)
-    }
-
-    deposit(val){
-        this._movements.push(val);
-    }
-    withdraw(val){
-        this.deposit(-val);
-    }
-
-    getMovements() {
-        return this._movements;
-    }
+function addBookToLibrary(title, author, pages) {
+    myLibrary.push(new Book(title, author, pages));
 }
-const me = new Account("robin reyes", "USD", 3695);
-me.deposit(1000);
-me.withdraw(100);
-me.withdraw(4);
-console.log(me.getMovements());
-
-console.log(me.locale);
-*/
-
-
-
 
